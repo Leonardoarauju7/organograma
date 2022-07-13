0081429e5fbd4797bb407f6ca1f2ebb288b9650a
Leonardo Araujo <leonardoearaujo@gmail.com>
2022-07-13 17:00:20 -0300

    Preparação de ambiente

---
+++ src/componentes/Banner.js
@@ -0,0 +1,11 @@
+import './Banner.css'
+import '../index.css'
+function Banner() {
+    return(
+        <header className="banner">
+            <img src="imagens/banner.png" alt=""></img>
+        </header>
+        
+    )
+}
+export default Banner
